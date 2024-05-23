68d7e92b57e2ea03b24bfa3a75cc16ace4d5445f
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2024-05-23 23:51:10 +0530

    docs: constant and assets added

---
+++ constants/Colors.js
@@ -0,0 +1,8 @@
+export default {
+  primary: "#3D38ED",
+  primaryMuted: "#C9C8FA",
+  background: "#F5F5F5",
+  dark: "#141518",
+  gray: "#626D77",
+  lightGray: "#D8DCE2",
+};
---
+++ constants/Styles.js
@@ -0,0 +1,61 @@
+import { StyleSheet } from "react-native";
+import Colors from "./Colors";
+
+export const defaultStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+    padding: 16,
+  },
+  header: {
+    fontSize: 40,
+    fontWeight: "700",
+  },
+  pillButton: {
+    padding: 10,
+    height: 60,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textLink: {
+    color: Colors.primary,
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  descriptionText: {
+    fontSize: 18,
+    marginTop: 20,
+    color: Colors.gray,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  pillButtonSmall: {
+    paddingHorizontal: 20,
+    height: 40,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonTextSmall: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  sectionHeader: {
+    fontSize: 20,
+    fontWeight: "bold",
+    margin: 20,
+    marginBottom: 10,
+  },
+  block: {
+    marginHorizontal: 20,
+    padding: 14,
+    backgroundColor: "#fff",
+    borderRadius: 16,
+    gap: 20,
+  },
+});
