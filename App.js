bb5ef7073131928a5fd0dd67269cd8d7fabdc71f
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2024-05-23 23:49:22 +0530

    chore: basic build setup

--- App.js
+++ App.js
@@ -1,20 +1,6 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import MyApp from "./src/MyApp";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to star</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <MyApp />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
