5e0d6ff291be04162d6573a1552affdb8675f8d7
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2024-05-25 23:40:17 +0530

    feat: otp verification screen added, clerk auth added

---
+++ src/screens/Verify.js
@@ -0,0 +1,114 @@
+import { Fragment, useEffect, useState } from "react";
+import Colors from "../../constants/Colors";
+import { defaultStyles } from "../../constants/Styles";
+import {
+  isClerkAPIResponseError,
+  useSignIn,
+  useSignUp,
+} from "@clerk/clerk-expo";
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from "react-native";
+import {
+  CodeField,
+  Cursor,
+  useBlurOnFulfill,
+  useClearByFocusCell,
+} from "react-native-confirmation-code-field";
+import styles from "./Verify.styles";
+
+const CELL_COUNT = 6;
+
+const Verify = ({ navigation, route }) => {
+  const { phone, signin } = route.params;
+  const [code, setCode] = useState("");
+  const { signIn } = useSignIn();
+  const { signUp, setActive } = useSignUp();
+  console.log("---phone---", phone);
+  const ref = useBlurOnFulfill({ value: code, cellCount: CELL_COUNT });
+  const [props, getCellOnLayoutHandler] = useClearByFocusCell({
+    value: code,
+    setValue: setCode,
+  });
+
+  useEffect(() => {
+    if (code.length === 6) {
+      if (signin === "true") {
+        verifySignIn();
+      } else {
+        verifyCode();
+      }
+    }
+  }, [code]);
+
+  const verifyCode = async () => {
+    try {
+      await signUp?.attemptPhoneNumberVerification({
+        code,
+      });
+      await setActive({ session: signUp?.createdSessionId });
+    } catch (err) {
+      console.log("error", JSON.stringify(err, null, 2));
+      if (isClerkAPIResponseError(err)) {
+        Alert.alert("Error", err.errors[0].message);
+      }
+    }
+  };
+
+  const verifySignIn = async () => {
+    try {
+      await signIn?.attemptFirstFactor({
+        strategy: "phone_code",
+        code,
+      });
+      await setActive({ session: signIn?.createdSessionId });
+    } catch (err) {
+      console.log("error", JSON.stringify(err, null, 2));
+      if (isClerkAPIResponseError(err)) {
+        Alert.alert("Error", err.errors[0].message);
+      }
+    }
+  };
+
+  return (
+    <View style={defaultStyles.container}>
+      <Text style={defaultStyles.header}>6-digit code</Text>
+      <Text style={defaultStyles.descriptionText}>
+        Code sent to {phone} unless you already have an account
+      </Text>
+
+      <CodeField
+        ref={ref}
+        {...props}
+        value={code}
+        onChangeText={setCode}
+        cellCount={CELL_COUNT}
+        rootStyle={styles.codeFieldRoot}
+        keyboardType="number-pad"
+        textContentType="oneTimeCode"
+        renderCell={({ index, symbol, isFocused }) => (
+          <Fragment key={index}>
+            <View
+              onLayout={getCellOnLayoutHandler(index)}
+              key={index}
+              style={[styles.cellRoot, isFocused && styles.focusCell]}
+            >
+              <Text style={styles.cellText}>
+                {symbol || (isFocused ? <Cursor /> : null)}
+              </Text>
+            </View>
+            {index === 2 ? (
+              <View key={`separator-${index}`} style={styles.separator} />
+            ) : null}
+          </Fragment>
+        )}
+      />
+
+      <TouchableOpacity onPress={() => navigation.navigate("Signin")}>
+        <Text style={[defaultStyles.textLink]}>
+          Already have an account? Log in
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Verify;
---
+++ src/screens/Verify.styles.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native";
+import Colors from "../../constants/Colors";
+
+const styles = StyleSheet.create({
+  codeFieldRoot: {
+    marginVertical: 20,
+    marginLeft: "auto",
+    marginRight: "auto",
+    gap: 12,
+  },
+  cellRoot: {
+    width: 45,
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.lightGray,
+    borderRadius: 8,
+  },
+  cellText: {
+    color: "#000",
+    fontSize: 36,
+    textAlign: "center",
+  },
+  focusCell: {
+    paddingBottom: 8,
+  },
+  separator: {
+    height: 2,
+    width: 10,
+    backgroundColor: Colors.gray,
+    alignSelf: "center",
+  },
+});
+
+export default styles;
