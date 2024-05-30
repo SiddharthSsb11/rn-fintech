6c32d1a96c83beb07c1374fd860c640f05435eb3
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2024-05-30 23:57:54 +0530

    feat: app configured with navigation store context & clerk auth

--- App.js
+++ App.js
@@ -1,6 +1,44 @@
 import "react-native-gesture-handler";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import MyApp from "./src/MyApp";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import * as SecureStore from "expo-secure-store";
+import { NavigationContainer } from "@react-navigation/native";
+import { UserInactivityProvider } from "./context/UserInactivity";
+
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+// Cache the Clerk JWT
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
 export default function App() {
-  return <MyApp />;
+  return (
+    <ClerkProvider
+      publishableKey={CLERK_PUBLISHABLE_KEY}
+      tokenCache={tokenCache}
+    >
+      <NavigationContainer>
+        <UserInactivityProvider>
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <MyApp />
+          </GestureHandlerRootView>
+        </UserInactivityProvider>
+      </NavigationContainer>
+    </ClerkProvider>
+  );
 }
